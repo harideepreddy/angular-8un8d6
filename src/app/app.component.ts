6e48f46fd22ab1edab220cd0702defa570bb5ea6
harideepreddy <harideepreddy2@gmail.com>
2019-11-10 22:42:07 +0530

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,19 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
-})
+  template: `
+  <h1>two way data binding in angular</h1>
+  <br/>
+  <input [value] = "name" (input) = 'name=$event.target.value'/>
+  <br/>
+  you have entered {{name}}
+  <br/>
+  <input [(ngModel)]="name1"/>
+  <br/>
+  you have entered {{name1}}
+  `
+  })
 export class AppComponent  {
-  name = 'Angular hi hari';
+  name = 'harideep';
+  name1 = 'hari';
 }
